4767f218f36d07defbad40129fad10b304af0c3b
Deep SenGupta 🥑 <myselfdeepsengupta@gmail.com>
2026-06-28 19:24:29 +0530

    Time: 0 ms (100%), Space: 19.3 MB (42.36%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -3,18 +3,18 @@ class Solution:
         i = 0
         n = len(s)
 
-        while i < n and s[i] == ' ':
+        while i < n and s[i] == " ":
             i += 1
 
         sign = 1
         if i < n and s[i] in "+-":
-            if s[i] == '-':
+            if s[i] == "-":
                 sign = -1
             i += 1
 
         num = 0
         while i < n and s[i].isdigit():
-            num = num * 10 + int(s[i])
+            num = num * 10 + ord(s[i]) - ord("0")
             i += 1
 
         num *= sign
