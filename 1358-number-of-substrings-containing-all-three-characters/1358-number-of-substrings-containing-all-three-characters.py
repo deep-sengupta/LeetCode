e8db00d3299d0351eb567b2bbbb3611fc1199101
Deep SenGupta 🥑 <myselfdeepsengupta@gmail.com>
2026-06-30 13:13:54 +0530

    Time: 111 ms (36.26%), Space: 19.6 MB (15.25%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -6,6 +6,7 @@ class Solution:
 
         for right in range(len(s)):
             count[s[right]] += 1
+
             while count['a'] and count['b'] and count['c']:
                 ans += len(s) - right
                 count[s[left]] -= 1
