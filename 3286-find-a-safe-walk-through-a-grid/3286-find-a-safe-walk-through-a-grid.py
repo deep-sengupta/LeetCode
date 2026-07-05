3cf9b3b71314da5b1acff3f152c5d06a35403248
Deep SenGupta 🥑 <myselfdeepsengupta@gmail.com>
2026-07-05 10:44:39 +0530

    Time: 50 ms (88.96%), Space: 19.7 MB (45.05%) - LeetHub

---
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+class Solution:
+    def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
+        m, n = len(grid), len(grid[0])
+        dist = [[float("inf")] * n for _ in range(m)]
+        start = grid[0][0]
+        dist[0][0] = start
+        dq = deque([(0, 0)])
+        while dq:
+            x, y = dq.popleft()
+            for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < m and 0 <= ny < n:
+                    w = grid[nx][ny]
+                    if dist[x][y] + w < dist[nx][ny]:
+                        dist[nx][ny] = dist[x][y] + w
+                        if w == 0:
+                            dq.appendleft((nx, ny))
+                        else:
+                            dq.append((nx, ny))
+        return dist[m - 1][n - 1] < health
