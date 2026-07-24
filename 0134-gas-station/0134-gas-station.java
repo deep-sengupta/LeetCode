db48e8e5ca0144d71bf8b5924a390fe018fab16f
Deep SenGupta 🥑 <myselfdeepsengupta@gmail.com>
2026-07-24 12:15:47 +0530

    Time: 2 ms (99.22%), Space: 119.4 MB (65.05%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int start = 0, tank = 0, total = 0;
+        for (int i = 0; i < gas.length; i++) {
+            int diff = gas[i] - cost[i];
+            tank += diff;
+            total += diff;
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        return total < 0 ? -1 : start;
+    }
+}
