b80996d1e18abe115dfd12760f574791c73d2900
Deep SenGupta 🥑 <myselfdeepsengupta@gmail.com>
2026-07-31 12:43:12 +0530

    Time: 99 ms (71.57%), Space: 20 MB (77.26%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,5 @@
+from collections import Counter
+
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        return sum(c*(i//8+1) for i,c in enumerate(sorted(Counter(word).values(), reverse=True)))
