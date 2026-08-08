fa58bda5c58f225ad8796d0eb366b95bd9dabe86
Deep SenGupta 🥑 <myselfdeepsengupta@gmail.com>
2026-08-08 11:53:33 +0530

    Time: 485 ms (63.04%), Space: 46.6 MB (86.96%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.py
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.py
@@ -1,24 +1,10 @@
 class Solution:
     def validSequence(self, a: str, b: str) -> list[int]:
-        n, m = len(a), len(b)
-        r = [-1] * m
-        j = m - 1
-
-        for i in range(n - 1, -1, -1):
-            if j >= 0 and a[i] == b[j]:
-                r[j] = i
-                j -= 1
-
-        ans = []
-        j = 0
-        changed = 0
-
-        for i, c in enumerate(a):
-            if j == m:
-                break
-            if c == b[j] or (not changed and (j == m - 1 or i < r[j + 1])):
-                ans.append(i)
-                changed |= c != b[j]
-                j += 1
-
-        return ans if j == m else []
+        r=[-1]*len(b);j=len(b)-1
+        for i in range(len(a)-1,-1,-1):
+            if j>=0 and a[i]==b[j]:r[j]=i;j-=1
+        ans=[];j=x=0
+        for i,c in enumerate(a):
+            if j<len(b) and (c==b[j] or (not x and (j+1==len(b) or i<r[j+1]))):
+                ans.append(i);x|=c!=b[j];j+=1
+        return ans if j==len(b) else []
